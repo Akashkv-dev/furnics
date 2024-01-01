924d2d11ac74fbed7ead1ec9c7e958f98afcb3ec
akashkv2438 <akashkv11@gmail.com>
2024-01-01 11:57:22 +0530

    first commit

---
+++ controller/userController.js
@@ -0,0 +1,8 @@
+module.exports={
+    loginpage:(req,res)=>{
+        res.render('users/login')
+    },
+    signpage:(req,res)=>{
+        res.render('users/signup')
+    }
+}
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+const { loginpage,signpage } = require('../controller/userController');
+// const { render } = require('../app');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/login',loginpage);
+router.get('/signup',signpage);
+
+
+module.exports = router;
