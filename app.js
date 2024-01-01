27d3cda4dd95b216472ea2c8fe6b23a4ea6f3727
akashkv2438 <akashkv11@gmail.com>
2024-01-01 12:36:10 +0530

    data base modify

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var session = require('express-session')
 var exphbs = require('express-handlebars')
 var dotenv = require('dotenv').config()
+var connect = require('./config/mongoConnect')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
