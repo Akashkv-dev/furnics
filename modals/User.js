e757a3171af061ba041b76eef96698861ac95450
akashkv2438 <akashkv11@gmail.com>
2024-01-01 19:00:15 +0530

    update

---
+++ modals/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const userSchema=new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phone:{
+
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    zip:{
+        type:Number,
+        required:true
+    }
+
+
+})
+module.exports=mongoose.model('users',userSchema)
