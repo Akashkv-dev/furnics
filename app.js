4fc48c02416c61c2dd5d06fc03a33319f9b62ea8
akashkv2438 <akashkv11@gmail.com>
2024-02-27 12:47:12 +0530

    mongo session

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(
     proxy: true,
     saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
-    store: new MongoDBStore({ mongooseConnection: connect }),
+    // store: new MongoDBStore({ mongooseConnection: connect }),
   })
 );
 
